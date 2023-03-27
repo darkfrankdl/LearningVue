ade9950658a6f14ab36b4e342cac5df0bf15463c
darkfrankdl <128466282+darkfrankdl@users.noreply.github.com>
2023-03-27 08:05:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
-
-// the follwing creates the vue application.
-
 const app = Vue.createApp({
 
-    // this is a datafuntion to print some data to the website dynamically
     data () {
         return {
-            // write here what to return from the data function
+            
             showBooks: true,
-            title: 'The final empire', // this is a title property
+            title: 'The final empire', 
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                {title: 'name of the wind' , author: 'Patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'the way of kings' , author: 'brandon sanderson',img: 'assets/2.jpg',isFav: false},
+                {title: 'the final empire' , author: 'brandon sanderson',img: 'assets/3.jpg',isFav: true}
+            ],
+            url: 'http://www.thenetninja.co.uk'
 
-        } // returns a data object // -> notice the short hand for a function on a object "function name () {}"
+        } 
     },
-    // component method:
-        // add a property inside the createApp object
-
     methods: {
-        // place for all methods for the component that should be run is placed here.
+        
         changeTitle (title) {
-            this.title = title // this refernces the component itself
+            this.title = title 
 
         },
         ToggleShowBooks()
         {
-            this.showBooks = !this.showBooks // ! reverses the boolean showBooks.
+            this.showBooks = !this.showBooks 
+        },
+        HandleEvent(event){
+            console.log(event);
+        },
+        HandleMousemove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        },
+        changeIsFav(book){
+            book.isFav = !book.isFav;
+            console.log(this.books.isFav)
+        }
+         
+
+    },
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav) 
         }
     }
+}); 
 
-
-    // the object passed into the createApp method is the root compoent that controls the #app section of the html
-    // these can be 
-    // data, functions, compenent template (that is rendered inside the app element)
-
-    //template: '<h2> I am a template </h2>'
-}); // this can be created because the lib for vue was imported inside the index.html file, thus a access to a vue object is granted
-
-app.mount('#app'); // this mounts or points to which part of the DOM that vue should control, the #app is the identifier for a specific section of the dom that we create. thus the app elemetn is in the index.html and vue controls everything inside the div tag
+app.mount('#app'); 
